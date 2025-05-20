cfd68bb2f3c821eafafd88f04e915eab1adb6367
NRD2019 <nostalgiaredrawn@gmail.com>
2025-05-20 02:44:36 -0500

    Initial website commit

---
+++ footer.js
@@ -0,0 +1,55 @@
+document.addEventListener('DOMContentLoaded', function() {
+  document.querySelector('footer').innerHTML = `
+    <div class="footer-container">
+      <div class="footer-column">
+        <h4>About Us</h4>
+        <ul>
+          <li><a href="about.html">Our Story</a></li>
+          <li><a href="about.html">Our Mission</a></li>
+          <li><a href="contact.html">Contact Us</a></li>
+        </ul>
+      </div>
+      
+      <div class="footer-column">
+        <h4>Sell to Us</h4>
+        <ul>
+          <li><a href="how-it-works.html">How It Works</a></li>
+          <li><a href="sell.html">Sell Your Supplies</a></li>
+          <li><a href="faqs.html">FAQs</a></li>
+        </ul>
+      </div>
+      
+      <div class="footer-column">
+        <h4>Resources</h4>
+        <ul>
+          <li><a href="privacy-policy.html">Privacy Policy</a></li>
+          <li><a href="terms-of-service.html">Terms of Service</a></li>
+        </ul>
+      </div>
+      
+      <div class="footer-column">
+        <h4>Get in Touch</h4>
+        <ul>
+          <li><a href="tel:+14054590973">405-459-0973</a></li>
+          <li><a href="mailto:webuydiabeticteststripsOK@gmail.com">webuydiabeticteststripsOK@gmail.com</a></li>
+          <li>1316 NW Sheridan Rd PMB 293<br>Lawton, OK 73505</li>
+        </ul>
+      </div>
+    </div>
+    
+    <div class="footer-bottom">
+      <div class="footer-logo">
+        <img src="assets/WBSTOK-Logo_3.png" alt="We Buy Test Strips Oklahoma Logo">
+      </div>
+      
+      <div class="copyright">
+        &copy; 2025 We Buy Test Strips Oklahoma. All rights reserved.
+      </div>
+      
+      <div class="social-icons">
+        <a href="https://www.facebook.com/OklahomaTestStrips/" target="_blank" aria-label="Follow us on Facebook"><i class="fab fa-facebook-f"></i></a>
+        <a href="https://www.instagram.com/wbtsok/" target="_blank" aria-label="Follow us on Instagram"><i class="fab fa-instagram"></i></a>
+      </div>
+    </div>
+  `;
+});
